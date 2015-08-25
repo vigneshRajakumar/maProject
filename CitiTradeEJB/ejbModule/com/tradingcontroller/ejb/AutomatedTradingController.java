e15a3ab782b2d8fa011605185863d223d85f42e3
Vignesh Rajakumar <vigneshr.93@gmail.com>
2015-08-25 09:30:03 +0800

    skeletom for automated tc session bean

---
+++ CitiTradeEJB/ejbModule/com/tradingcontroller/ejb/AutomatedTradingController.java
@@ -0,0 +1,57 @@
+package com.tradingcontroller.ejb;
+
+import java.util.ArrayList;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+
+import com.trade.jpa.Trade;
+
+/**
+ * Session Bean implementation class AutomatedTradingController
+ */
+@Stateful
+@LocalBean
+public class AutomatedTradingController {
+	private class bollingerStockWrapper {
+		private String stockSymbol;
+
+		public String getStockSymbol() {
+			return stockSymbol;
+		}
+
+		public void setStockSymbol(String stockSymbol) {
+			this.stockSymbol = stockSymbol;
+		}
+		
+	}
+
+	private ArrayList<bollingerStockWrapper> stockList;
+    /**
+     * Default constructor. 
+     */
+    public AutomatedTradingController() {
+        // TODO Auto-generated constructor stub
+    	
+    }
+    
+    public void AddStockToTrade(String stock) {
+    	
+    }
+
+	public void RemoveStockToTrade(String stock) {
+		
+	}
+	
+	public void RecordTrade(Trade trade) {
+		
+	}
+
+	public ArrayList<bollingerStockWrapper> getStockList() {
+		return stockList;
+	}
+
+	public void setStockList(ArrayList<bollingerStockWrapper> stockList) {
+		this.stockList = stockList;
+	}
+}

47910ca67d042bdb0aa0000c8eb0f60d0ade1eb1
cuteCute <tan.wei.jie.sg@gmail.com>
2015-08-28 00:45:33 +0800

    commit for pull

--- CitiTradeEJB/ejbModule/com/twomovingaverages/ejb/TwoMovingAverages.java
+++ CitiTradeEJB/ejbModule/com/twomovingaverages/ejb/TwoMovingAverages.java
@@ -20,97 +20,72 @@ public class TwoMovingAverages implements ITwoMovingAverages{
 	private MarketDataHandlerLocal marketDataHandler;
 
 	ArrayList<TMAObjClass> storeToMonitor = storeToMonitor = new ArrayList<TMAObjClass>();
-	
+
 	private final static int REPEAT_CYCLE_TIME = 2000 ;
 	Timer timer = null;
 
 	private class MonitorPrice extends TimerTask {
 
 		public void run() {			
-			
+
 			for (TMAObjClass obj : storeToMonitor){
-								
+
 				System.out.println(obj.getStockSymbol());
-				
+
 				Stock newInfo = marketDataHandler.getStockBySymbol(obj.getStockSymbol());
 				double newStockAvg = newInfo.getAsk() + newInfo.getBid();
-				
+
 				obj.setShortCounter(obj.getShortCounter()+1);
-				
+
 				double newShortAvg = (obj.getShortAvg() + newStockAvg)	/ obj.getShortCounter();
-				
+
 				obj.setShortAvg(newShortAvg);
-				
+
 				if (obj.getShortAvg() < obj.getLongAvg()){ //if short average line is below long average  (GOING FOR LONG POSITION)
-				
+
 					if (!obj.isBuy() && obj.getShortAvg() >= obj.getLongAvg()){ //if haven buy and just pass the long avg,quickly buy it
-						
+
 						obj.setBuy(true);;
 						double getBuyPrice = newInfo.getAsk();
-						obj.setBuyPrice(getBuyPrice);		
-						
-						
-						
-						
+						obj.setBuyPrice(getBuyPrice);	
+
 					} else if (obj.isBuy()) { //bought already and check for 1 percent profit
 
 						if (obj.getShortAvg() >= (obj.getBuyPrice() * 1.1)){
-							
+
 							obj.setBuy(false); 
-							//sell here(or remove from trade) and get the profit 
-							
-							
-							
-							
-							
-						} else if (obj.getShortAvg() <= (obj.getBuyPrice() * 1.1)){ //bought already and check for 1 percent profit loss, exit here
-							
-							//sell here(or remove from trade) and make a loss
-							
-							
-							
-							
+
+						} else if (obj.getShortAvg() <= (obj.getBuyPrice() * 1.1)){ //bought already and check for 1 percent profit loss, exit here	
+
 						}
 					}
-					
+
 				} else if (obj.getLongAvg() < obj.getShortAvg()){ //if short average line is higher than long average (Going for SHORT POSITION)
-					
+
 					if (!obj.isSell() && obj.getShortAvg() <= obj.getLongAvg()){ //if it is not sold yet, sell it 
-						
+
 						obj.setSell(true);
 						obj.setSellPrice(newInfo.getBid());
-
-						//vignesh insert into new trade here, (SHORT POSITION)
-
-						
-						
-						
-						
-
 					} else if (obj.isSell()) { //sold already and check for 1 percent profit
 
 						if (obj.getShortAvg() <= (obj.getSellPrice() * 1.1)){ //if there is profit
-							
+
 							obj.setSell(false);
-							//buy here(or remove from trade) and get the profit 
-							
-							
-						
-						
+
 						} else if (obj.getShortAvg() >= (obj.getSellPrice() * 1.1)){ //if there is loss, exit here
-													
+
 							obj.setSell(false) ;
 							//buy here(or remove from trade) and get the loss 
-							
-							
-							
-							
-							
+
+
+
+
+
 						}
 					}				
-					
+
 				}
-				
+
 			}
 		}
 	}
@@ -131,8 +106,8 @@ public class TwoMovingAverages implements ITwoMovingAverages{
 
 		TMAObjClass tmaObj = new TMAObjClass(obj);
 		monitorPrice();
-		
-		
+
+
 		storeToMonitor.add(tmaObj);
 
 
